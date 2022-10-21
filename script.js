ac36c2a347050c78219995fc77d56aca64cf83aa
Nell <eemandersson@outlook.com>
2022-10-21 09:06:08 +0200

    buttonstyling, multiple question js

--- script.js
+++ script.js
@@ -24,37 +24,47 @@ darkModeToggle.addEventListener("click", () => {
 
 // ------------------------------- QUESTIONS ------------------------------- //
 
-const getQuestionElement = (question, index) => {
+const getRadioInputElement = (inputName, inputValue, labelText) => {
+	const wrapperElement = document.createElement("div");
+	wrapperElement.classList.add("form-check");
+	const inputElement = document.createElement("input");
+	inputElement.type = "radio";
+	inputElement.setAttribute("name", inputName);
+	inputElement.value = inputValue;
+	const labelElement = document.createElement("label");
+	labelElement.classList.add("form-label");
+	labelElement.innerText = labelText;
+	wrapperElement.append(inputElement);
+	wrapperElement.append(labelElement);
+	return wrapperElement;
+};
+
+const getTrueFalseQuestionElement = (question, index) => {
 	const parent = document.createElement("div");
 	parent.classList.add("question");
 	const para = document.createElement("p");
 	para.classList.add("question-text");
 	para.innerText = `${index + 1}. ${question.text}`;
 	parent.append(para);
-	const trueOption = document.createElement("div");
-	trueOption.classList.add("form-check");
-	const trueInput = document.createElement("input");
-	trueInput.type = "radio";
-	trueInput.setAttribute("name", `q${index + 1}`);
-	trueInput.value = "A";
-	const trueLabel = document.createElement("label");
-	trueLabel.classList.add("form-label");
-	trueLabel.innerText = "True";
-	trueOption.append(trueInput);
-	trueOption.append(trueLabel);
-	const falseOption = document.createElement("div");
-	falseOption.classList.add("form-check");
-	const falseInput = document.createElement("input");
-	falseInput.type = "radio";
-	falseInput.setAttribute("name", `q${index + 1}`);
-	falseInput.value = "B";
-	const falseLabel = document.createElement("label");
-	falseLabel.classList.add("form-label");
-	falseLabel.innerText = "False";
-	falseOption.append(falseInput);
-	falseOption.append(falseLabel);
-	parent.append(trueOption);
-	parent.append(falseOption);
+	const trueInput = getRadioInputElement(`q${index + 1}`, "A", "True");
+	const falseInput = getRadioInputElement(`q${index + 1}`, "B", "False");
+	parent.append(trueInput);
+	parent.append(falseInput);
+	return parent;
+};
+
+const getMultipleChoiceQuestionElement = (question, index) => {
+	const parent = document.createElement("div");
+	parent.classList.add("question");
+	const para = document.createElement("p");
+	para.classList.add("question-text");
+	para.innerText = `${index + 1}. ${question.text}`;
+	parent.append(para);
+
+	question.choices.forEach((choice) => {
+		const inputElement = getRadioInputElement(`q${index + 1}`, choice.value, choice.label);
+		parent.append(inputElement);
+	});
 	return parent;
 };
 
@@ -70,58 +80,81 @@ const getColorFromScore = (score) => {
 	return "#bb2323";
 };
 
+const questionTypes = {
+	trueFalse: "truefalse",
+	multiple: "multiple",
+};
+
 // Lista med frågor
 
 const questions = [
 	{
-		text: "Första frågan?",
+		text: "Vem är längst?",
 		answer: "A",
-		type: "trueorfalse",
+		choices: [
+			{
+				label: "Patrick",
+				value: "A",
+			},
+			{
+				label: "Nell",
+				value: "B",
+			},
+			{
+				label: "Floki",
+				value: "C",
+			},
+			{
+				label: "Tonto",
+				value: "D",
+			},
+		],
+		type: questionTypes.multiple,
 	},
 	{
 		text: "Andra Frågan?",
 		answer: "B",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Trejde Frågan?",
 		answer: "A",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Fjärde Frågan?",
 		answer: "B",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Femte Frågan?",
 		answer: "A",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Sjätte Frågan?",
 		answer: "B",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Sjunde Frågan?",
 		answer: "A",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Åttonde Frågan?",
 		answer: "B",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Nionde Frågan?",
 		answer: "A",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 	{
 		text: "Tionde Frågan?",
 		answer: "B",
-		type: "trueorfalse",
+		type: questionTypes.truefalse,
 	},
 ];
 
@@ -130,8 +163,14 @@ const form = document.querySelector(".quiz-form");
 // För varje fråga, hämta ett element för den frågan och lägg till i html-formuläret
 
 questions.forEach((question, index) => {
-	const questionElement = getQuestionElement(question, index);
-	form.append(questionElement);
+	if (question.type === questionTypes.truefalse) {
+		const questionElement = getTrueFalseQuestionElement(question, index);
+		form.append(questionElement);
+	}
+	if (question.type === questionTypes.multiple) {
+		const questionElement = getMultipleChoiceQuestionElement(question, index);
+		form.append(questionElement);
+	}
 });
 
 // Efter frågorna, skapa submitknapp
