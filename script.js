8805a94ee15c1755ac31d6c4efcd6298b60c3e68
Nell <eemandersson@outlook.com>
2022-10-24 17:02:16 +0200

    Added checkboxtype answer

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ darkModeToggle.addEventListener("click", () => {
 
 // ------------------------------- QUESTIONS ------------------------------- //
 
-const getRadioInputElement = (inputName, inputValue, labelText) => {
+const getInputElement = (inputName, inputValue, labelText, type) => {
 	const wrapperElement = document.createElement("div");
 	wrapperElement.classList.add("form-check");
 	const inputElement = document.createElement("input");
-	inputElement.type = "radio";
+	inputElement.type = type;
 	inputElement.setAttribute("name", inputName);
 	inputElement.value = inputValue;
 	const labelElement = document.createElement("label");
@@ -46,8 +46,8 @@ const getTrueFalseQuestionElement = (question, index) => {
 	para.classList.add("question-text");
 	para.innerText = `${index + 1}. ${question.text}`;
 	parent.append(para);
-	const trueInput = getRadioInputElement(`q${index + 1}`, "A", "True");
-	const falseInput = getRadioInputElement(`q${index + 1}`, "B", "False");
+	const trueInput = getInputElement(`q${index + 1}`, "A", "True", "radio");
+	const falseInput = getInputElement(`q${index + 1}`, "B", "False", "radio");
 	parent.append(trueInput);
 	parent.append(falseInput);
 	return parent;
@@ -62,7 +62,22 @@ const getMultipleChoiceQuestionElement = (question, index) => {
 	parent.append(para);
 
 	question.choices.forEach((choice) => {
-		const inputElement = getRadioInputElement(`q${index + 1}`, choice.value, choice.label);
+		const inputElement = getInputElement(`q${index + 1}`, choice.value, choice.label, "radio");
+		parent.append(inputElement);
+	});
+	return parent;
+};
+
+const getCheckboxQuestionElement = (question, index) => {
+	const parent = document.createElement("div");
+	parent.classList.add("question");
+	const para = document.createElement("p");
+	para.classList.add("question-text");
+	para.innerText = `${index + 1}. ${question.text}`;
+	parent.append(para);
+
+	question.choices.forEach((choice) => {
+		const inputElement = getInputElement(`q${index + 1}`, choice.value, choice.label, "checkbox");
 		parent.append(inputElement);
 	});
 	return parent;
@@ -83,6 +98,7 @@ const getColorFromScore = (score) => {
 const questionTypes = {
 	trueFalse: "truefalse",
 	multiple: "multiple",
+	checkbox: "checkbox",
 };
 
 // Lista med frågor
@@ -112,9 +128,27 @@ const questions = [
 		type: questionTypes.multiple,
 	},
 	{
-		text: "Andra Frågan?",
+		text: "Vilken karaktär är med i Attack on Titan?",
 		answer: "B",
-		type: questionTypes.truefalse,
+		choices: [
+			{
+				label: "Eren",
+				value: "A",
+			},
+			{
+				label: "Askeladd",
+				value: "B",
+			},
+			{
+				label: "Floki",
+				value: "C",
+			},
+			{
+				label: "Thor",
+				value: "D",
+			},
+		],
+		type: questionTypes.multiple,
 	},
 	{
 		text: "Trejde Frågan?",
@@ -152,9 +186,27 @@ const questions = [
 		type: questionTypes.truefalse,
 	},
 	{
-		text: "Tionde Frågan?",
-		answer: "B",
-		type: questionTypes.truefalse,
+		text: "Tionde Frågan",
+		answer: ["B", "A"],
+		choices: [
+			{
+				label: "Hej1",
+				value: "A",
+			},
+			{
+				label: "Hej2",
+				value: "B",
+			},
+			{
+				label: "Hej3",
+				value: "C",
+			},
+			{
+				label: "Hej4",
+				value: "D",
+			},
+		],
+		type: questionTypes.checkbox,
 	},
 ];
 
@@ -163,6 +215,10 @@ const form = document.querySelector(".quiz-form");
 // För varje fråga, hämta ett element för den frågan och lägg till i html-formuläret
 
 questions.forEach((question, index) => {
+	if (question.type === questionTypes.checkbox) {
+		const questionElement = getCheckboxQuestionElement(question, index);
+		form.append(questionElement);
+	}
 	if (question.type === questionTypes.truefalse) {
 		const questionElement = getTrueFalseQuestionElement(question, index);
 		form.append(questionElement);
@@ -184,8 +240,11 @@ buttonInput.classList.add("btn-submit");
 buttonWrapper.append(buttonInput);
 form.append(buttonWrapper);
 
+// Saker
+
 const result = document.querySelector(".result-container");
 const hidequestion = document.querySelector(".container");
+const hideanswer = document.querySelector(".answer-container");
 
 // Resultat
 
@@ -193,17 +252,38 @@ form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	let score = 0;
-
+	let answerString = "";
 	questions.forEach((question, index) => {
-		const formAnswer = form[`q${index + 1}`].value;
-		if (formAnswer === question.answer) {
-			score += 100 / questions.length;
+		if (question.type === questionTypes.checkbox) {
+			const checkboxes = document.querySelectorAll(`input[name="q${index + 1}"]:checked`);
+			let values = [];
+			checkboxes.forEach((checkbox) => {
+				values.push(checkbox.value);
+			});
+			isCorrect = JSON.stringify(values.sort()) === JSON.stringify(question.answer.sort());
+
+			if (isCorrect) {
+				score += 100 / questions.length;
+				answerString += `${index + 1}. ${question.text} : Rätt\n`;
+			} else {
+				answerString += `${index + 1}. ${question.text} : Fel\n`;
+			}
+		} else {
+			const formAnswer = form[`q${index + 1}`].value;
+			if (formAnswer === question.answer) {
+				score += 100 / questions.length;
+				answerString += `${index + 1}. ${question.text} : Rätt\n`;
+			} else {
+				answerString += `${index + 1}. ${question.text} : Fel\n`;
+			}
 		}
 	});
-
-	console.log(result.querySelector("span"), `${score}%`);
 	result.classList.remove("hidden");
 	hidequestion.classList.add("hidden");
+	hideanswer.classList.remove("hidden");
+
+	const answerTextElement = document.querySelector(".answer-text");
+	answerTextElement.innerText = answerString;
 
 	let output = 0;
 	const timer = setInterval(() => {
